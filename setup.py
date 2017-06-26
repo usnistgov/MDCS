230ab90bf66da11b818f8549fed472a48ada4cbc
Adrien Catel <adrien.catel@nist.gov>
2017-06-26 10:11:12 -0400

    add core_federated_search_app in requirement + change version mdcs to 2.0.0-alpha2
    Change-Id: I051f8565ad6c8d6370f860a8f24fe8b1296568c0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="mdcs",
-    version="1.0.0-alpha2",
+    version="2.0.0-alpha2",
     description=("Materials Data Curation System package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",

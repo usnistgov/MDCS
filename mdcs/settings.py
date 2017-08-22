064d546b0c0ca56859ff17fbdbd749f6d921d42f
Adrien Catel <adrien.catel@nist.gov>
2017-08-22 15:41:39 -0400

    Fix Celery
    Change-Id: Ifdb10f792e5c4a09489bb662492102511d59eb9f

--- mdcs/settings.py
+++ mdcs/settings.py
@@ -7,11 +7,9 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 from mongoengine.connection import connect
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production

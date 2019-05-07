f3de77223f5b8badf88e91d5ef2bf416e49ec864
Augustin Chini <augustin.chini@NIST.GOV>
2019-05-07 16:23:02 -0400

    fix: GUI shaking on specific screen size
    Change-Id: Idfe3dc242e7da29447ba5ef2da8c1619ffdc38c2

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,28 +1,7 @@
 // Dropdowns.
-var navbar;
-var windowElement = $(window);
-$( document ).ready(function(){
-    navbar = $('#nav');
-});
-
 $('#nav > ul').dropotron({
     expandMode: "click",
     offsetY: -15,
     hoverDelay: 0,
     hideDelay: 500
 });
-
-windowElement.scroll(debounce(function (event) {
-    if(windowElement.scrollTop() > 0){
-        navbar.css(
-            {
-                position: 'fixed',
-                top: 0
-            });
-    } else {
-                navbar.css(
-            {
-                position: 'relative'
-            });
-    }
-}, 100));

3c27db422a9bfe81e636b0d3cc47c0f209b90ac8
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:50 -0400

    feat: upgrade django 2.2
    Change-Id: I333bb0e7affb8dbc278124548d259a505c300ec5

--- mdcs_home/views.py
+++ mdcs_home/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.shortcuts import render
 
 from core_main_app.components.template_version_manager.api import get_global_version_managers

5d4575527cd05f89ca3267f55f2a2d4ee5d4ec45
Philippe Dessauw <philippe.dessauw@nist.gov>
2020-10-22 09:47:23 -0400

    fix: update mdcs settings for PID
    Change-Id: Ic4033df8763089837cbc8d153c05f4c566333280

--- mdcs/settings.py
+++ mdcs/settings.py
@@ -480,8 +480,16 @@ ID_PROVIDER_SYSTEMS = {
         "class": "core_linked_records_app.utils.providers.local.LocalIdProvider",
         "args": [],
     },
+    # "handle": {
+    #     "class": "core_linked_records_app.utils.providers.handle_net.HandleNetSystem",
+    #     "args": [
+    #         "https://handle-server.example.org:8000",
+    #         "300%3APREFIX/USER",
+    #         "password",
+    #     ],
+    # },
 }
-""" dict: all the provider system available for registring PIDs
+""" dict: provider systems available for registring PIDs.
 """
 
 ID_PROVIDER_PREFIXES = ["cdcs"]

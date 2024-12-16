d5fec8de9cb5959a75891410ef6b9278eacb27d2
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-12-16 09:49:53 -0500

    feat: add token auth
    Change-Id: I591208bd28abdec85565aaba1655c637334f8721

--- mdcs/settings.py
+++ mdcs/settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = (
     "oauth2_provider",
     # Extra apps
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_spectacular",
     "menu",
     "captcha",
@@ -261,6 +262,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
